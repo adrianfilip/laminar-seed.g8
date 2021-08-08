d7e1a54ee4927b120e52948694c3470e0fac94da
Adrian Filip <realadrianfilip@gmail.com>
2021-08-08 15:49:48 +0300

    Fix g8 $ character issues

--- src/main/g8/vite.config.js
+++ src/main/g8/vite.config.js
@@ -4,23 +4,23 @@ import {minifyHtml, injectHtml} from 'vite-plugin-html'
 const scalaVersion = '3.0.1'
 
 export default ({mode}) => {
-    const mainJS = `./target/scala-${scalaVersion}/laminar-hello-${mode === 'production' ? 'opt' : 'fastopt'}\/main.js`
-    const script = `<script type="module" src="${mainJS}"></script>`
+	const mainJS = "./target/scala-\${scalaVersion}/laminar-hello-\${mode === 'production' ? 'opt' : 'fastopt'}/main.js"
+	const script = `<script type="module" src="\${mainJS}"></script>`
 
-    return {
-        publicDir: './src/main/static/public',
-        plugins: [
-            ...(process.env.NODE_ENV === 'production' ? [minifyHtml(),] : []),
-            injectHtml({
-                injectData: {
-                    script
-                }
-            })
-        ],
-        resolve: {
-            alias: {
-                'stylesheets': resolve(__dirname, './src/main/static/stylesheets'),
-            }
-        }
-    }
+	return {
+		publicDir: './src/main/static/public',
+		plugins: [
+			...(process.env.NODE_ENV === 'production' ? [minifyHtml(),] : []),
+			injectHtml({
+				injectData: {
+					script
+				}
+			})
+		],
+		resolve: {
+			alias: {
+				'stylesheets': resolve(__dirname, './src/main/static/stylesheets'),
+			}
+		}
+	}
 }
